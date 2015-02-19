d23a8c603a72f734dfafbff4b22294d4d5e0513d
Katie Hoffman <ktahoffman@gmail.com>
2015-02-19 12:14:22 -0500

    remove passing code

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,13 +1,1 @@
 # your code goes here
-
-class Ticket
-
-  attr_reader :origin, :destination, :name
-
-  def initialize(origin, destination, name)
-    @origin = origin
-    @destination = destination
-    @name = name
-  end
-
-end
