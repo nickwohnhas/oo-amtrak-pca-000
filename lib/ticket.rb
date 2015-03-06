19f3fe96fe39aafa6398ab77981df58bfd434dbb
Katie Hoffman <ktahoffman@gmail.com>
2015-03-06 11:57:57 -0500

    merge with master, add passing code

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,1 +1,11 @@
-# code the ticket class here
+class Ticket
+
+  attr_reader :origin, :destination, :name
+
+  def initialize(origin, destination, name)
+    @origin = origin
+    @destination = destination
+    @name = name
+  end
+
+end
